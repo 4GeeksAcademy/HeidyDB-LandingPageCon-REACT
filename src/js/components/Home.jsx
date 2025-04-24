234e73dbae1388feeccdabb9482d2030e4c95cee
HeidyDB <heidydiazbarrios@gmail.com>
2025-04-24 16:03:27 +0000

    actualizacion en grilla y columan a la vez

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -4,8 +4,6 @@ import Card from "./Card";
 import CardWellcome from "./CardWellcome";
 import Footer from "./Footer";
 
-//include images into your bundle
-//import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
@@ -15,29 +13,47 @@ const Home = () => {
 
 	return (
 		<>
+		<div className="text-center dropdown-moviles">
 			<Navbar />	
-
-			<div className = " container">		
-				
-					<CardWellcome />
-				
-				
-					<div className="row row-cols-1 row-cols-sm-1 row-cols-md-4 gx-4">
-
+			<div className = "container text-center">		
+			<CardWellcome />	
+			
+					<div className="  col-12 col-sm-6 col-md-4 col-lg-3 mb-4  justify-content-center"> 
+						
 						<Card img="https://th.bing.com/th/id/OIP._z4OGDUks1r6gU7ZndH5WAHaFH?rs=1&pid=ImgDetMain" title="Tomate un buen cafe..." description="Estimula cada neurona y genera energia. Un cafe hace tu dia mas lindo " />
 						<Card img="https://th.bing.com/th/id/R.7e14b34a9a9e9c40513e49e2d08f89d7?rik=HWB4P4JkMd4Kog&riu=http%3a%2f%2f3.bp.blogspot.com%2f-fG_d6eQzhFQ%2fUM82TqV7nKI%2fAAAAAAAAAQA%2fvbCEayI-M8k%2fs1600%2f45%2b(5).jpg&ehk=ueAu%2b6SPAu3CN8NfrPiPqRzfifN%2bTs7fz%2fu8XYhv45A%3d&risl=&pid=ImgRaw&r=0 " title="Baila bajo la lluvia" description="No dejes que nada te haga perder el buen buen humor." />
 						<Card img="https://i.pinimg.com/736x/20/3b/d8/203bd8ea729eaad24ebf94bc2ac149a8--sea-turtles-maui.jpg" title="Nada siempre que puedas" description="Nadar no solo ejercita tu cuerpo sino tambien tu mente" />
 						<Card img="https://floweraura-blog-img.s3.ap-south-1.amazonaws.com/christmas-2023/secret-santa-ideas/Polaroid-Camera-D.jpg" title="Viaja y captura el momento" description="Siempre ten un viaje en mente. Esto es buena señal de que aun te sientes joven. " />
 
 					</div>
-
-
 			</div>
-
 			<Footer />	
+		</div>
 
+
+<div >ahora en 4 columnas</div>
+
+		<div className="text-center"> 
+			<Navbar />	
+			<div className = "container">		
+			<CardWellcome />	
+			
+					<div className="row row-cols-sm-6  row-cols-lg-4 gx-4"> 
+						
+						<Card img="https://th.bing.com/th/id/OIP._z4OGDUks1r6gU7ZndH5WAHaFH?rs=1&pid=ImgDetMain" title="Tomate un buen cafe..." description="Estimula cada neurona y genera energia. Un cafe hace tu dia mas lindo " />
+						<Card img="https://th.bing.com/th/id/R.7e14b34a9a9e9c40513e49e2d08f89d7?rik=HWB4P4JkMd4Kog&riu=http%3a%2f%2f3.bp.blogspot.com%2f-fG_d6eQzhFQ%2fUM82TqV7nKI%2fAAAAAAAAAQA%2fvbCEayI-M8k%2fs1600%2f45%2b(5).jpg&ehk=ueAu%2b6SPAu3CN8NfrPiPqRzfifN%2bTs7fz%2fu8XYhv45A%3d&risl=&pid=ImgRaw&r=0 " title="Baila bajo la lluvia" description="No dejes que nada te haga perder el buen buen humor." />
+						<Card img="https://i.pinimg.com/736x/20/3b/d8/203bd8ea729eaad24ebf94bc2ac149a8--sea-turtles-maui.jpg" title="Nada siempre que puedas" description="Nadar no solo ejercita tu cuerpo sino tambien tu mente" />
+						<Card img="https://floweraura-blog-img.s3.ap-south-1.amazonaws.com/christmas-2023/secret-santa-ideas/Polaroid-Camera-D.jpg" title="Viaja y captura el momento" description="Siempre ten un viaje en mente. Esto es buena señal de que aun te sientes joven. " />
+
+					</div>
+			</div>
+			<Footer />	
+		</div>
 		</>
 	);
 };
 
 export default Home;
+//row row-cols-sm-6  row-cols-lg-4 gx-4
+//  col-12 col-sm-6 col-md-4 col-lg-3 mb-4"
+
--- src/js/components/Navbar.jsx
+++ src/js/components/Navbar.jsx
@@ -1,47 +1,39 @@
 import React from "react";
 
 const Navbar = () => {
-    return (
- 
-/*<ul className="navbar justify-content-end navbar-expand-lg  bg-dark">
-  <li className="nav-item">
-    <a className="nav-link style.color: white active" aria-current="page" href="#">Active</a>
-  </li>
-  <li className="nav-item">
-    <a className="nav-link " href="#">Link</a>
-  </li>
-  <li className="nav-item">
-    <a className="nav-link" href="#">Link</a>
-  </li>
-  <li className="nav-item">
-    <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-  </li>
-</ul>*/
+  return (
 
-
-        <nav className="navbar navbar-expand-lg  justify-content-end  navbar-dark bg-dark">
-            <div className="container-fluid">
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+    <nav className="navbar navbar justify-content-end  navbar-expand-lg  justify-content-end  navbar-dark bg-dark">
+      <div className="container-fluid">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"> </span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <a className="navbar-brand" href="#">Start Boostrap</a>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <li className="nav-item dropdown">
+                <button class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                  Tipo de dispositivo
                 </button>
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                    <a className="navbar-brand" href="#">Home</a>
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Service</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Contact</a>
-                        </li>
-                    </ul>
-                  </div>
-            </div>
-        </nav>
+                <ul className="dropdown-menu dropdown-menu-dark">
+                  <li><a className="dropdown-item" href="#">Moviles</a></li>
+                  <li><a className="dropdown-item" href="#">Portatiles</a></li>
+                </ul>
+              </li>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Acerca de </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 
-    )
+  )
 }
 
 
